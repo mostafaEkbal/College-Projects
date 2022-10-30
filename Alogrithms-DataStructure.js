4db831daeb7f4dbcd1be29a31cb00c8db71df7c1
Mostafa Ekbal <mostafa.mekbal@gmail.com>
2022-10-30 23:52:17 +0200

    finished building the max heap algorithm

--- Alogrithms-DataStructure.js
+++ Alogrithms-DataStructure.js
@@ -1,23 +1,45 @@
-let array = [54, 26, 93, 17, 77, 31, 44, 55, 20];
+let array = [54, 26, 93, 17, 77, 31, 44, 55, 20, 69, 88, 99, 101];
 
 const swap = (array = [], i = 0, j = 1) => {
   [array[i], array[j]] = [array[j], array[i]];
 };
 
-const maxHeapify = (array, i) => {
-  const l = 2 * i;
-  const r = 2 * i + 1;
-  let largest;
-  array[l] > array[r] ? (largest = l) : (largest = r);
-  if (array[i] > array[l] && array[i] > array[r]) {
-    return;
+const shiftDown = (array, i) => {
+  const arrayInternalNodes = Math.floor(array.length / 2);
+  if (i * 2 > arrayInternalNodes) {
+    maxHeapify(array, i);
+    return array;
   }
-  swap(array, i, largest);
+  maxHeapify(array, i);
+  for (let j = 2 * i; j <= 2 * i + 1; j++) {
+    maxHeapify(array, j);
+  }
+};
+
+const maxHeapify = (array, i) => {
+  const l = 2 * i - 1;
+  const r = 2 * i;
+  const index = i - 1;
+  let largest = index;
+  if (array[index] < array[l]) {
+    largest = l;
+  }
+  if (array[largest] < array[r]) {
+    largest = r;
+  }
+  if (largest === index) {
+    return array;
+  }
+  swap(array, index, largest);
+  if (r <= Math.floor(array.length / 2)) {
+    shiftDown(array, i);
+  }
+  return array;
 };
 
 const buildMaxHeap = array => {
-  for (i = Math.floor(array.length / 2 - 1); i >= 1; i--) {
-    maxHeapify(array, i);
+  for (let i = Math.floor(array.length / 2); i >= 1; i--) {
+    shiftDown(array, i);
   }
   return array;
 };
