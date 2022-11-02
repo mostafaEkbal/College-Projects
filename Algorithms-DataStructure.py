ac1c50662d2da404b21491b2711f035614db7a85
Mostafa Ekbal <mostafa.mekbal@gmail.com>
2022-11-02 04:08:21 +0200

    Quick Select Searching algorithm

--- Algorithms-DataStructure.py
+++ Algorithms-DataStructure.py
@@ -1,36 +1,41 @@
-""" def insertionSort(alist):
-    for index in range(1, len(alist)):
-
-        currentvalue = alist[index]
-        position = index
-
-        while position > 0 and alist[position-1] > currentvalue:
-            alist[position] = alist[position-1]
-            position = position-1
-
-        alist[position] = currentvalue
-
-"""
+import random
 alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 
 
-def binarySearch(alist, item):
-    if len(alist) == 0:
-        return False
+def quickSelect(items, itemIndex):
 
-    midpoint = len(alist) // 2
-    if alist[midpoint] == item:
-        return True
+    def select(list, l, r, index):
+        # base case
+        if (r == 1):
+            return list[l]
 
-    if alist[midpoint] > item:
-        return binarySearch(alist[:midpoint], item)
+        # choose random pivot number
+        pivotIndex = random.randint(l, r)
 
-    if alist[midpoint] < item:
-        return binarySearch(alist[midpoint + 1:], item)
+        # move the pivot to the begining of the list
+        list[pivotIndex], list[l] = list[l], list[pivotIndex]
+
+        # partition
+        i = l
+        for j in range(l + 1, r + 1):
+            if (list[j] < list[l]):
+                i += 1
+                list[i], list[j] = list[j], list[i]
+
+        # move pivot to the correct location
+        list[l], list[i] = list[i], list[l]
+
+        # recursively partition one side
+        if (index == i):
+            return list[i]
+        if (i > index):
+            return select(list, l, i - 1, index)
+        if (index > i):
+            return select(list, i + 1, r, index)
+
+    return select(items, 0, len(items) - 1, itemIndex)
 
 
-sorted(alist)
+value = quickSelect(alist, 8)
 
-found = binarySearch(alist, 93)
-
-print(found)
+print(value)
