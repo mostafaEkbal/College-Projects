0860cb073e34e57fd3b179a0bf606dfdd6c2be88
Mostafa Ekbal <mostafa.mekbal@gmail.com>
2022-11-26 15:10:19 +0200

    box sorting with bubble sort algorithm

--- Algorithms-DataStructure.py
+++ Algorithms-DataStructure.py
@@ -1,41 +1,24 @@
-import random
-alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+array = [(6, 3, 8), (7, 2, 3), (5, 1, 3)]
 
 
-def quickSelect(items, itemIndex):
-
-    def select(list, l, r, index):
-        # base case
-        if (r == 1):
-            return list[l]
-
-        # choose random pivot number
-        pivotIndex = random.randint(l, r)
-
-        # move the pivot to the begining of the list
-        list[pivotIndex], list[l] = list[l], list[pivotIndex]
-
-        # partition
-        i = l
-        for j in range(l + 1, r + 1):
-            if (list[j] < list[l]):
-                i += 1
-                list[i], list[j] = list[j], list[i]
-
-        # move pivot to the correct location
-        list[l], list[i] = list[i], list[l]
-
-        # recursively partition one side
-        if (index == i):
-            return list[i]
-        if (i > index):
-            return select(list, l, i - 1, index)
-        if (index > i):
-            return select(list, i + 1, r, index)
-
-    return select(items, 0, len(items) - 1, itemIndex)
+def swap(j, n):
+    n[j], n[j+1] = n[j+1], n[j]
 
 
-value = quickSelect(alist, 8)
+def boxSorting(n):
+    hauteur = 0
+    swapped = False
+    for i in range(len(n) - 1, 0, -1):
+        hauteur += n[i][1]
+        for j in range(i):
+            if (n[j][0] * n[j][2] > n[j+1][0] * n[j+1][2]):
+                swap(j, n)
+                swapped = True
 
-print(value)
+        if not swapped:
+            return n
+    return n
+
+
+print(array)
+print(boxSorting(array))
