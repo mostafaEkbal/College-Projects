50d363e9afa700443b479efc80a08a118f547372
Mostafa Ekbal <mostafa.mekbal@gmail.com>
2022-11-30 17:11:48 +0200

    writing down the heap sort algorithm

--- Algorithms-DataStructure.py
+++ Algorithms-DataStructure.py
@@ -1,27 +1,56 @@
 array = [(6, 3, 8), (7, 2, 3), (5, 1, 3)]
 
 
-def swap(j, n):
-    n[j], n[j+1] = n[j+1], n[j]
+def swap(a, i, j):
+    a[i], a[j] = a[j], a[i]
 
 
-def boxSorting(n):
-    swapped = False
-    print('From Bottom')
-    for i in range(len(n) - 1, 0, -1):
-        for j in range(i):
-            if (n[j][0] * n[j][2] > n[j+1][0] * n[j+1][2]):
-                swap(j, n)
-                swapped = True
-        print(n[i])
-
-        if not swapped:
-            return n
-
-        if (i == 1):
-            print(n[0])
-            print('To Top')
-    return n
+def isHeap(a):
+    n = 0
+    m = 0
+    while True:
+        for i in [0, 1]:
+            m += 1
+            if m >= len(a):
+                return True
+            if a[m] > a[n]:
+                return False
+        n += 1
 
 
-boxSorting(array)
+def shiftDown(a, n, max):
+    while True:
+        biggest = n
+        c1 = 2*n + 1
+        c2 = c1 + 1
+        for c in [c1, c2]:
+            if c < max and a[c] > a[biggest]:
+                biggest = c
+            if biggest == c:
+                return a
+
+            swap(a, n, biggest)
+            n = biggest
+
+
+def heapify(a):
+    i = int(len(a) / 2 - 1)
+    max = len(a)
+    while i >= 0:
+        shiftDown(a, i, max)
+        i -= 1
+
+
+def heapSort(a):
+    heapify(a)
+    for j in range(len(a) - 1, 0, -1):
+        swap(a, 0, j)
+        shiftDown(a, 0, j)
+
+
+a = [12, 11, 15, 8, 9, 20, 77, 5, 2]
+
+print(a)
+
+heapSort(a)
+print(a)
