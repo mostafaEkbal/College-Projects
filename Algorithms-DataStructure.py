dffd2cabc176aab1ff84e7cd54714336195a7e16
Mostafa Ekbal <mostafa.mekbal@gmail.com>
2022-12-05 20:47:34 +0200

    finished the second question

--- Algorithms-DataStructure.py
+++ Algorithms-DataStructure.py
@@ -1,54 +1,38 @@
-# Python program for implementation of heap Sort
+def differenceBetweenAge(a):
+    newArr = []
+    nextLargest = 0
+    nextSmallest = 0
+    n = len(a)
+    for i in range(n):
+        j = i+1
+        if i > 0:
+            if a[i] - a[i-1] > 0 and a[nextLargest] - a[i] > 0:
+                newArr.append(a[nextLargest] - a[i])
+                continue
+            if a[nextSmallest] - a[i] > 0:
+                newArr.append(a[nextSmallest] - a[i])
+                continue
+        while True:
+            if j > n - 1:
+                j = 0
 
-# To heapify subtree rooted at index i.
-# n is size of heap
+            if j == i:
+                newArr.append(-1)
+                nextLargest = 0
+                nextSmallest = 0
+                break
 
+            if a[i] < a[j]:
+                newArr.append(a[j] - a[i])
+                nextLargest = j
+                break
 
-def heapify(arr, N, i):
-    largest = i  # Initialize largest as root
-    l = 2 * i + 1     # left = 2*i + 1
-    r = 2 * i + 2     # right = 2*i + 2
-
-# See if left child of root exists and is
-# greater than root
-    if l < N and arr[largest][0] * arr[largest][1] < arr[l][0] * arr[l][1]:
-        largest = l
-
-# See if right child of root exists and is
-# greater than root
-    if r < N and arr[largest][0] * arr[largest][1] < arr[r][0] * arr[r][1]:
-        largest = r
-
-# Change root, if needed
-    if largest != i:
-        arr[i], arr[largest] = arr[largest], arr[i]  # swap
-
-    # Heapify the root.
-        heapify(arr, N, largest)
-
-# The main function to sort an array of given size
-
-
-def heapSort(arr):
-    N = len(arr)
-
-# Build a maxheap.
-    for i in range(N//2 - 1, -1, -1):
-        heapify(arr, N, i)
-# One by one extract elements
-    for i in range(N-1, 0, -1):
-        arr[i], arr[0] = arr[0], arr[i]  # swap
-        heapify(arr, i, 0)
+            if a[i] > a[j]:
+                nextSmallest = j
+            j += 1
+    return newArr
 
 
-# Driver's code
-if __name__ == '__main__':
-    arr = [(6, 8), (2, 4), (7, 1), (9, 2), (5, 7)]
+arr = [11, 12, 9, 11, 13, 7, 12, 9, 10, 15, 8]
 
-# Function call
-    heapSort(arr)
-    N = len(arr)
-
-    print("Sorted array is", str(arr))
-
-    # This code is contributed by Mohit Kumra
+print(differenceBetweenAge(arr))
