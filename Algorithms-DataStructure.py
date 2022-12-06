cb67e6f726684108c2730000debc1f67d6ff546a
Mostafa Ekbal <mostafa.mekbal@gmail.com>
2022-12-06 20:31:40 +0200

    third question algorithm implemented

--- Algorithms-DataStructure.py
+++ Algorithms-DataStructure.py
@@ -1,14 +1,44 @@
-a = [5, 7, 8, 11]
-b = [8, 5, 11, 7]
-c = [11, 7, 5, 8]
+import time
+a = [140, 120, 150, 100, 170, 200, 90, 180]
+b = [170, 150, 140, 90, 100, 120, 180, 200]
+c = [120, 90, 200, 150, 180, 140, 100, 170]
 
-for i in range(len(a) // 2 - 1):
-    for j in range(len(b) // 2 - 1):
-        for w in range(len(c) // 2):
-            if 2*w+2 >= len(c):
-                if a[w] == b[2*w+1] == c[j]:
-                    print('A[', w + 1, '] with B[', 2*w+2, '] with C[', j+1, ']')
-                continue
 
-            if a[w] == b[2*w+1] == c[2*w+2]:
-                print('A[', w + 1, '] with B[', 2*w+2, '] with C[', 2*w+3, ']')
+def bruteForce(a, b, c):
+    for i in range(len(a)):
+        for j in range(len(b)):
+            for w in range(len(c)):
+                if a[i] == b[j] == c[w]:
+                    print('A[', i + 1, '] with B[', j +
+                          1, '] with C[', w + 1, ']')
+
+
+def dynamic(a, b, c):
+    for i in range(len(a)):
+        found = False
+        for j in range(len(b)):
+            if found:
+                break
+            for w in range(len(c)):
+                if a[i] != b[j]:
+                    break
+                if a[i] == b[j] == c[w]:
+                    print('A[', i + 1, '] with B[', j +
+                          1, '] with C[', w + 1, ']')
+                    found = True
+                    break
+
+
+start = time.time()
+
+bruteForce(a, b, c)
+
+end = time.time()
+print(end - start)
+
+start = time.time()
+
+dynamic(a, b, c)
+
+end = time.time()
+print(end - start)
