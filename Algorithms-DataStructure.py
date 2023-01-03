b6bd11e08f947ba84083536636e10810b645f1d5
Mostafa Ekbal <mostafa.mekbal@gmail.com>
2023-01-03 23:21:30 +0200

    Building a hash table for integers

--- Algorithms-DataStructure.py
+++ Algorithms-DataStructure.py
@@ -1,7 +1,33 @@
-def hashTable(a):
-    newArr = [None] * len(a)
-    for i in range(len(a)):
-        if newArr[a[i] % len(a)] != None:
-            newArr[a[i] % len(a) + 1] = a[i]
-            continue
-        newArr[a[i] % len(a)] = a[i]
+import random
+alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+
+
+def hashKey(value, arraySize):
+    return value % arraySize
+
+
+def initHashTable(hashTableSize):
+    hashTable = {}
+    for i in range(hashTableSize):
+        hashTable[i] = None
+    return hashTable
+
+
+table = initHashTable(10)
+
+
+def insertInTable(item, hashTable):
+    if hashTable[hashKey(item, len(hashTable))]:
+        hashTable[hashKey(item, len(hashTable))].append(item)
+        return
+    hashTable[hashKey(item, len(hashTable))] = [item]
+
+
+def insertArrayInTable(array, hashTable):
+    for item in array:
+        insertInTable(item, hashTable)
+
+
+insertArrayInTable(alist, table)
+
+print(table)
