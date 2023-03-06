5a4c2b10cf36cc1fac0d330857fddef0695f055d
Mostafa Ekbal <mostafa.mekbal@gmail.com>
2023-03-07 00:35:33 +0200

    Collections Class methods test

--- OOP/Main.java
+++ OOP/Main.java
@@ -2,29 +2,54 @@ package OOP;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> cityList = new ArrayList<String>();
-        cityList.add("London");
-        cityList.add("Paris");
-        cityList.add("Denver");
-        cityList.add("Miami");
-        cityList.add("Tokyo");
-        cityList.add("Seoul");
-        System.out.println(cityList);
-        System.out.println(cityList.size());
-        System.out.println(cityList.contains("Miami"));
-        System.out.println(cityList.indexOf("Denver"));
-        cityList.add(4, "Paris");
-        cityList.remove("Paris");
-        String[] cityArray = converArrayListToArray(cityList);
-        System.out.println("This is an Array " + Arrays.toString(cityArray) + "\nAnd this is an ArrayList " + cityList);
+        Integer[] array1 = { 90, 70, 88, 66, 94, 93 };
+        ArrayList<Integer> list1 = converIntegerArrayToArrayList(array1);
+        collectionsMethodsTest(list1);
     }
 
-    static String[] converArrayListToArray(ArrayList<String> arrayList) {
+    static void arrayListMethodsTest(ArrayList<String> arrayList) {
+        arrayList.add("London");
+        arrayList.add("Paris");
+        arrayList.add("Denver");
+        arrayList.add("Miami");
+        arrayList.add("Tokyo");
+        arrayList.add("Seoul");
+        System.out.println(arrayList);
+        System.out.println(arrayList.size());
+        System.out.println(arrayList.contains("Miami"));
+        System.out.println(arrayList.indexOf("Denver"));
+        arrayList.add(4, "Paris");
+        arrayList.remove("Paris");
+    }
+
+    static void collectionsMethodsTest(ArrayList<Integer> arraylist) {
+        System.out.println(arraylist);
+        Collections.reverse(arraylist);
+        System.out.println(arraylist);
+        System.out.println(Collections.max(arraylist));
+        System.out.println(Collections.min(arraylist));
+        Integer sum = 0;
+        for (Integer num : arraylist)
+            sum += num;
+        System.out.println(sum);
+        Collections.shuffle(arraylist);
+        System.out.println(arraylist);
+        Collections.sort(arraylist);
+        System.out.println(arraylist);
+    }
+
+    static String[] converStringArrayListToArray(ArrayList<String> arrayList) {
         String[] array = new String[arrayList.size()];
         arrayList.toArray(array);
         return array;
+    }
+
+    static ArrayList<Integer> converIntegerArrayToArrayList(Integer[] array) {
+        ArrayList<Integer> arrayList = new ArrayList<Integer>(Arrays.asList(array));
+        return arrayList;
     }
 }
