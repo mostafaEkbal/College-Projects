ce9a90c6fc5a128867788983466a1b368d4d5515
Mostafa Ekbal <mostafa.mekbal@gmail.com>
2023-03-14 01:05:08 +0200

    initialized WebTechnologies directory

---
+++ WebTechnologies/regex.ts
@@ -0,0 +1,11 @@
+// A pattern that matches two letters only string
+const pattern1: RegExp = /^[a-zA-z]{2}$/;
+
+// A pattern that matches a string that begins with four numbers and end with four letters
+const pattern2: RegExp = /^\d{4}[a-zA-Z]{4}$/;
+
+// any number of any characters followed by 2 digits
+const pattern3: RegExp = /^.+\d{2}$/;
+
+// any digit, followed by any number of digits, followed by exactly 3 letters
+const pattern4: RegExp = /^\d\d*[a-zA-Z]{3}/;
