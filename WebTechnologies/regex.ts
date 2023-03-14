15922a94d4f22a31bb8dfdc5f30a5c7b66e0217d
Mostafa Ekbal <mostafa.mekbal@gmail.com>
2023-03-14 02:06:34 +0200

    adding more regex patterns

--- WebTechnologies/regex.ts
+++ WebTechnologies/regex.ts
@@ -9,3 +9,9 @@ const pattern3: RegExp = /^.+\d{2}$/;
 
 // any digit, followed by any number of digits, followed by exactly 3 letters
 const pattern4: RegExp = /^\d\d*[a-zA-Z]{3}/;
+
+// A pattern for card numbers(Visa, MasterCard, etc..)
+const cardPattern: RegExp = /(?:\d{4}\-){3}(\d{4}$)/;
+
+// A pattern for an email adress
+const emailPattern: RegExp = /^[\w\-\.\+]+\@\w+\.\w{2,4}$/;
