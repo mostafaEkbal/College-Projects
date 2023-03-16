f074d66f64bc19d23cba266cf5eaaf9f1899b5b5
Mostafa Ekbal <mostafa.mekbal@gmail.com>
2023-03-16 06:28:17 +0200

    creating a table with array data

--- WebTechnologies/src/index.php
+++ WebTechnologies/src/index.php
@@ -1,3 +1,36 @@
-<?php 
-echo "Hello World";
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>First PHP Site</title>
+</head>
+
+<body>
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Occupation</th>
+        </tr>
+
+        <?php
+        $occupations = array(
+            "K. Smith" => 'Plumber',
+            'N. Jones' => 'Electrican',
+            'P. Ibboston' => 'Plasterer'
+        );
+
+        foreach ($occupations as $name => $occupation) {
+            echo '<tr>';
+            echo '<td>', htmlspecialchars(print_r($name, true)), '</td>';
+            echo '<td>', htmlspecialchars(print_r($occupation, true)), '</td>';
+            echo '</tr>';
+        }
+        ?>
+    </table>
+</body>
+
+</html>
