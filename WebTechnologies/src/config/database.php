65d105e032751247235da3af5f7e6def0124a616
Mostafa Ekbal <mostafa.mekbal@gmail.com>
2023-03-29 01:50:47 +0200

    connecting to a mysql database

---
+++ WebTechnologies/src/config/database.php
@@ -0,0 +1,15 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_USER", "ekbal");
+define("DB_PASS", "123456");
+define("DB_NAME", "php_dev");
+function connectDatabase($host, $user, $pass, $name)
+{
+    $conn = new mysqli($host, $user, $pass, $name);
+    if ($conn->connect_error) {
+        die("Connection Failed" . $conn->connect_errno);
+    }
+    echo "Connected successfully";
+}
+connectDatabase(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+?>
