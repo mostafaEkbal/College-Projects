dcbbf3313f34d4c667d53fe27b0ede4cf1b4e213
Mostafa Ekbal <mostafa.mekbal@gmail.com>
2023-03-30 03:46:55 +0200

    writing the sumbit data function

--- WebTechnologies/src/index.php
+++ WebTechnologies/src/index.php
@@ -4,6 +4,12 @@
 $sql = 'SELECT * FROM sales';
 $result = mysqli_query($conn, $sql);
 $sales = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+$clientName = $_POST['client-name'];
+$date = $_POST['date'];
+$amount = $_POST['amount'];
+$sumbit_query = "INSERT INTO sales(clientName, date, amount) VALUES ('$clientName', '$date', '$amount') ";
+mysqli_query($conn, $sumbit_query);
 ?>
 
 <!DOCTYPE html>
@@ -18,7 +24,7 @@ $sales = mysqli_fetch_all($result, MYSQLI_ASSOC);
 </head>
 
 <body>
-    <form action="">
+    <form action="functions/post_data.php">
         <h2>Sumbit New Sales Record</h2>
         <label for="client-name">Client Name</label>
         <input type="text" name="client-name">
@@ -27,7 +33,7 @@ $sales = mysqli_fetch_all($result, MYSQLI_ASSOC);
         <label for="amount">Value of Sale</label>
         <input type="number" name="amount" pattern="[0-9]+">
     </form>
-    <form action="">
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
         <h2>Produce a Sorted Display of Sales</h2>
         <label for="sort-on">Sort tabel on = </label>
         <select>
