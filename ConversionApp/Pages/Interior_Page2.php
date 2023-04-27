5f0b077b390897a0c01452e679f71cd3907c514b
Mostafa Ekbal <mostafa.mekbal@gmail.com>
2023-04-27 12:56:22 +0200

    finished the conversion algorithm

--- ConversionApp/Pages/Interior_Page2.php
+++ ConversionApp/Pages/Interior_Page2.php
@@ -1,15 +1,15 @@
 <main class="conversion">
     <h2>Conversion Tool</h2>
-    <form action="" class="conversion__form">
+    <form class="conversion__form">
         <div class="conversion__units-container">
             <label class="conversion__label" for="convert-from">from:</label>
             <select name="convert-from" id="convert-from" class="conversion__select">
                 <option value="null">...</option>
                 <option value="bit" class="converion__option">Bit</option>
-                <option value="byte" class="converion__option">Bytte</option>
+                <option value="byte" class="converion__option">Byte</option>
                 <option value="kiloByte" class="converion__option">KiloByte</option>
                 <option value="megaByte" class="converion__option">MegaByte</option>
-                <option value="gegaByte" class="converion__option">GegaByte</option>
+                <option value="gigaByte" class="converion__option">GigaByte</option>
             </select>
         </div>
         <div class="conversion__units-container">
@@ -17,15 +17,15 @@
             <select name="convert-to" id="convert-to" class="conversion__select">
                 <option value="null">...</option>
                 <option value="bit" class="converion__option">Bit</option>
-                <option value="byte" class="converion__option">Bytte</option>
+                <option value="byte" class="converion__option">Byte</option>
                 <option value="kiloByte" class="converion__option">KiloByte</option>
                 <option value="megaByte" class="converion__option">MegaByte</option>
-                <option value="gegaByte" class="converion__option">GegaByte</option>
+                <option value="gigaByte" class="converion__option">GigaByte</option>
             </select>
         </div>
         <div class="conversion__input-container">
             <input type="number" placeholder="enter a value" id="number-input">
-            <button type="submit">convert</button>
+            <button type="submit" id="conversion-sumbit">convert</button>
         </div>
         <div class="conversion__output" id="conversion-output">
 
--- ConversionApp/js/app.js
+++ ConversionApp/js/app.js
@@ -1,22 +1,98 @@
 const convertFrom = document.getElementById('convert-from');
 const convertTo = document.getElementById('convert-to');
 const numberInput = document.getElementById('number-input');
-const output = document.getElementById('conversion-input');
+const sumbit = document.getElementById('conversion-sumbit');
+const output = document.getElementById('conversion-output');
 
 function dataUnitsConversion(from, to, input) {
-  if (from.value !== 'null' && to.value !== 'null') return;
-  if (!input.value) return;
+  let err = false;
+  if (from.value === 'null' || to.value === 'null') {
+    err = true;
+    return 'the selection is empty';
+  }
+  if (!input.value) {
+    err = true;
+    return 'the input is empty';
+  }
+  if (from.value === to.value) {
+    err = true;
+    return 'select different units';
+  }
   let result;
+
   if (from.value === 'bit' && to.value === 'byte') {
-    result = input / 8;
+    result = input.value / 8;
   }
   if (from.value === 'bit' && to.value === 'kiloByte') {
-    result = ((input / 8) * 2) ^ -10;
+    result = input.value / 8 / 2 ^ 10;
   }
   if (from.value === 'bit' && to.value === 'megaByte') {
-    result = ((input / 8) * 2) ^ -20;
+    result = input.value / 8 / 2 ^ 20;
   }
   if (from.value === 'bit' && to.value === 'gigaByte') {
-    result = ((input / 8) * 2) ^ -30;
+    result = input.value / 8 / 2 ^ 30;
   }
+
+  if (from.value === 'byte' && to.value === 'bit') {
+    result = input.value * 8;
+  }
+  if (from.value === 'byte' && to.value === 'kiloByte') {
+    result = input.value / 2 ^ 10;
+  }
+  if (from.value === 'byte' && to.value === 'megaByte') {
+    result = input.value / 2 ^ 20;
+  }
+  if (from.value === 'byte' && to.value === 'gigaByte') {
+    result = input.value / 2 ^ 30;
+  }
+  if (from.value === 'bit' && to.value === 'byte') {
+    result = input.value / 8;
+  }
+
+  if (from.value === 'kiloByte' && to.value === 'megaByte') {
+    result = input.value / 2 ^ 10;
+  }
+  if (from.value === 'kiloByte' && to.value === 'gigaByte') {
+    result = input.value / 2 ^ 20;
+  }
+  if (from.value === 'kiloByte' && to.value === 'byte') {
+    ((result = input.value) * 2 ^ 10;
+  }
+  if (from.value === 'kiloByte' && to.value === 'bit') {
+    result = input.value * 8 * 2 ^ 10;
+  }
+
+  if (from.value === 'megaByte' && to.value === 'kiloByte') {
+    result = input.value * 2 ^ 10;
+  }
+  if (from.value === 'megaByte' && to.value === 'byte') {
+    result = input.value * 2 ^ 20;
+  }
+  if (from.value === 'megaByte' && to.value === 'gigaByte') {
+    result = input.value / 2 ^ 10;
+  }
+  if (from.value === 'megaByte' && to.value === 'bit') {
+    ((result = input.value * 8) * 2 ^ 20;
+  }
+
+  if (from.value === 'gigaByte' && to.value === 'kiloByte') {
+    result = input.value * 2 ^ 20;
+  }
+  if (from.value === 'gigaByte' && to.value === 'megaByte') {
+    result = input.value * 2 ^ 10;
+  }
+  if (from.value === 'gigaByte' && to.value === 'byte') {
+    result = input.value * 2 ^ 30;
+  }
+  if (from.value === 'gigaByte' && to.value === 'bit') {
+    result = input.value * 8 * 2 ^ 30;
+  }
+  return `The result: ${input.value} ${from.value} = ${result} ${to.value} `;
 }
+
+sumbit.onclick = e => {
+  e.preventDefault();
+  const result = dataUnitsConversion(convertFrom, convertTo, numberInput);
+  output.textContent = result;
+  numberInput.value = '';
+};
