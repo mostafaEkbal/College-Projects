a33ca021a448e0f81909d5e152bd5e059043cfdc
Mostafa Ekbal <mostafa.mekbal@gmail.com>
2023-05-07 06:41:57 +0300

    destructive operations on sets

--- OOP/Main.java
+++ OOP/Main.java
@@ -1,20 +1,22 @@
 package OOP;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.LinkedHashSet;
-import java.io.*;
-import java.util.Scanner;
 import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        Set<String> herbs = new LinkedHashSet<String>();
-        herbs.add("rose");
-        herbs.add("parsley");
-        herbs.add("camomile");
-        System.out.println(herbs.size());
-        System.out.println(herbs);
+        Integer[] array1 = { 3, 4, 5, 2, 1 };
+        Integer[] array2 = { 2, 3, 7, 8, 9 };
+        Set<Integer> s1 = new LinkedHashSet<Integer>(Arrays.asList(array1));
+        Set<Integer> s2 = new LinkedHashSet<Integer>(Arrays.asList(array2));
+        System.out.println(s1);
+        System.out.println(s2);
+        s1.addAll(s2);
+        System.out.println(s1);
+        s1.retainAll(s2);
+        System.out.println(s1);
+        s1.removeAll(s2);
+        System.out.println(s1);
     }
 }
