f8f2b3241da463faf59144214e204896d42f3b28
Mostafa Ekbal <mostafa.mekbal@gmail.com>
2023-05-17 19:42:48 +0300

    Created a simple Circle Controller app

--- OOP/javafxapp/src/main/java/com/ekbal/App.java
+++ OOP/javafxapp/src/main/java/com/ekbal/App.java
@@ -1,9 +1,14 @@
 package com.ekbal;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,18 +22,33 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        FlowPane container = new FlowPane(Orientation.VERTICAL, 0, 20);
+        container.setAlignment(Pos.CENTER);
+        HBox hbox = new HBox();
+        hbox.setAlignment(Pos.CENTER);
+        Circle circle = new Circle();
+        circle.setRadius(20);
+        circle.setFill(Color.YELLOWGREEN);
+        circle.setStroke(Color.AZURE);
+        hbox.getChildren().add(0, circle);
+
+        HBox btnContainer = new HBox(5);
+        Button btnEnlarge = new Button("Enlarge");
+        Button btnShrink = new Button("Shrink");
+        btnContainer.getChildren().addAll(btnEnlarge, btnShrink);
+        container.getChildren().addAll(hbox, btnContainer);
+
+        btnEnlarge.setOnAction(e -> {
+            circle.setRadius(circle.getRadius() + 2);
+        });
+        btnShrink.setOnAction(e -> {
+            circle.setRadius(circle.getRadius() - 2);
+        });
+
+        scene = new Scene(container, 500, 400);
         stage.setScene(scene);
+        stage.setTitle("revolutionary Project");
         stage.show();
-    }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
