5ce7b6f1dca11d5846d395de50f9b8eaacb3c470
Mostafa Ekbal <mostafa.mekbal@gmail.com>
2023-05-18 11:37:55 +0300

    Implemnting Threads with Runnable Interface

--- OS/Task.java
+++ OS/Task.java
@@ -1,19 +1,31 @@
 package OS;
 
 public class Task implements Runnable {
+    private int n;
+
+    public Task(int n) {
+        this.n = n;
+    }
+
+    @Override
     public void run() {
-        System.out.println("I am a thread");
+        for (int i = 0; i < 100; i++) {
+            System.out.println("I am in Thread " + n);
+        }
     }
 
     public static void main(String[] args) {
-        WhereAmI place1 = new WhereAmI(2);
-        WhereAmI place2 = new WhereAmI(1);
-        WhereAmI place3 = new WhereAmI(3);
+        Task place1 = new Task(2);
+        Thread thread1 = new Thread(place1);
+        Task place2 = new Task(1);
+        Thread thread2 = new Thread(place2);
+        Task place3 = new Task(3);
+        Thread thread3 = new Thread(place3);
 
         try {
-            place1.start();
-            place2.start();
-            place3.start();
+            thread1.start();
+            thread2.start();
+            thread3.start();
         } catch (Exception ie) {
             System.out.println(ie);
         }
