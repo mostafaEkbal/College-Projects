aff805ad5b3dab9f55d1cee1d0420bd98ec8f261
Mostafa Ekbal <mostafa.mekbal@gmail.com>
2023-05-23 14:23:12 +0300

    revisiting sets

--- OOP/Main.java
+++ OOP/Main.java
@@ -11,14 +11,17 @@ import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) {
-        String text = "Good morning. Have a good class. " + "Have a good visit. Have fun!";
-        Map<String, Integer> sortedWordsCount = new TreeMap<>(countTheOccurenceOfWords(text)S);
-        System.out.println(sortedWordsCount);
+        // String text = "Good morning. Have a good class. " + "Have a good visit. Have
+        // fun!";
+        // Map<String, Integer> sortedWordsCount = new
+        // TreeMap<>(countTheOccurenceOfWords(text));
+        // System.out.println(sortedWordsCount);
+        bulkOp();
     }
 
     static Map<String, Integer> countTheOccurenceOfWords(String text) {
         Map<String, Integer> wordsCount = new HashMap<>();
-        String[] words = text.split("\\W+");
+        String[] words = text.trim().split("\\W+");
 
         for (String word : words) {
             word = word.toLowerCase();
@@ -33,5 +36,20 @@ public class Main {
             wordsCount.put(word, 1);
         }
         return wordsCount;
+    }
+
+    static void bulkOp() {
+        Integer[] array1 = { 3, 4, 5, 2, 1 };
+        Integer[] array2 = { 2, 3, 7, 8, 9 };
+        Set<Integer> s1 = new LinkedHashSet<Integer>(Arrays.asList(array1));
+        Set<Integer> s2 = new LinkedHashSet<Integer>(Arrays.asList(array2));
+        System.out.println(s1);
+        System.out.println(s2);
+        s1.addAll(s2);
+        System.out.println(s1);
+        s1.retainAll(s2);
+        System.out.println(s1);
+        s1.removeAll(s2);
+        System.out.println(s1);
     }
 }
