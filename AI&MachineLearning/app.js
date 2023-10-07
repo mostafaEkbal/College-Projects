7b2197ddaefa48254d8404814e96b04f4932a338
Mostafa Ekbal <mostafa.mekbal@gmail.com>
2023-10-08 01:30:32 +0300

    Representing the Graph in AdjanceyList

--- AI&MachineLearning/app.js
+++ AI&MachineLearning/app.js
@@ -1,5 +1,17 @@
-let nodes = [];
+let adjanceyList = new Map();
 
-for(let i = 1; i<=32; i++) {
-    nodes.push(i);
-}
+adjanceyList.set(1, [2, 3, 4]);
+adjanceyList.set(2, [5, 6, 7, 8]);
+adjanceyList.set(3, [9, 10]);
+adjanceyList.set(4, [11, 12, 13]);
+adjanceyList.set(5, [14]);
+adjanceyList.set(6, [15, 16, 17]);
+adjanceyList.set(7, [18, 19, 20, 21]);
+adjanceyList.set(8, [22, 23, 24]);
+adjanceyList.set(9, [25]);
+adjanceyList.set(10, [26]);
+adjanceyList.set(11, [27, 28]);
+adjanceyList.set(12, [29, 30]);
+adjanceyList.set(13, [31, 32]);
+
+console.log(adjanceyList);
