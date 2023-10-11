7e04c371cbbc28f33676c9dcd75b0cf3b7fb83f4
Mostafa Ekbal <mostafa.mekbal@gmail.com>
2023-10-11 05:17:41 +0300

    Dfs traversal

--- AI&MachineLearning/app.js
+++ AI&MachineLearning/app.js
@@ -52,4 +52,82 @@ function bfs(start, target) {
   }
 }
 
-bfs(1, 31);
+function dfs(graph, startNode, goalNode) {
+  // Create a stack to store the nodes to be visited.
+  const stack = [startNode];
+
+  // Create a visited set to keep track of the nodes that have already been visited.
+  const visited = new Set();
+
+  // Create a paths object to store the paths to each node.
+  const paths = {[startNode]: [startNode]};
+  printStackAndPaths(stack, paths);
+
+  // While the stack is not empty, keep searching.
+  while (stack.length > 0) {
+    // Get the next node from the stack.
+    const node = stack.pop();
+
+    // If we have reached the goal node, return the path to it.
+    if (node === goalNode) {
+      return console.log('goal node found!', node);
+    }
+
+    // Mark the node as visited.
+    visited.add(node);
+
+    if (!graph[node - 1]) {
+      stack.pop();
+      continue;
+    }
+    // Add the node's children to the stack, in reverse order, if they have not already been visited.
+    for (const child of graph[node - 1].reverse()) {
+      if (!visited.has(child)) {
+        stack.push(child);
+        paths[child] = [...paths[node], child];
+      }
+    }
+
+    // Print the stack and the paths only for the nodes currently in the stack.
+    printStackAndPaths(stack, paths);
+  }
+
+  // If we reach this point, the goal node was not found.
+  return null;
+}
+
+function printStackAndPaths(stack, paths) {
+  console.log('Stack:');
+  for (const node of stack) {
+    console.log(`  ${node}`);
+  }
+
+  console.log('Paths:');
+  for (const node of stack) {
+    console.log(`  ${node}: ${paths[node]}`);
+  }
+}
+
+// Create the graph.
+const graph = [
+  [2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10],
+  [11, 12, 13],
+  [14],
+  [15, 16, 17],
+  [18, 19, 20, 21],
+  [22, 23, 24],
+  [25],
+  [26],
+  [27, 28],
+  [29, 30],
+  [31, 32],
+];
+
+// Start the DFS search at node 1 and search for node 31.
+const startNode = 1;
+const goalNode = 31;
+
+// Perform the DFS search.
+dfs(graph, startNode, goalNode);
